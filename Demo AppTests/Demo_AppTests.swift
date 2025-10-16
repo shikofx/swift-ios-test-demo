9e8623f6972ba36c00ed07851932d7e8edfc47ce
Дмитрий Пархейчук <shikofx@gmail.com>
2025-10-16 07:21:39 +0300

    TEST: Add test target

---
+++ Demo AppTests/Demo_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Demo_AppTests.swift
+//  Demo AppTests
+//
+//  Created by d parkheychuk on 16.10.25.
+//
+
+import Testing
+
+struct Demo_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
